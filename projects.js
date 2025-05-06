005b18503fba400ed180c2b4c422742792f9d169
Valeria Aguilar <Valesophiagui@gmail.com>
2025-05-06 11:29:32 -0400

    Add files via upload

---
+++ projects.js
@@ -0,0 +1,31 @@
+export const projectsInfo = [
+    {
+        projectName: 'Research',
+        projectImage: 'wave.jpg',
+        projectDesc: 'I have completed 1 research project. I have 40 research hours in total!',
+        projectLink: 'research.html',
+    },
+    {
+        projectName: 'Internship',
+        projectImage: 'wave.jpg',
+        projectDesc: 'Description',
+        projectLink: 'internship.html',
+    },
+    {
+        projectName: 'Volunteering',
+        projectImage: 'wave.jpg',
+        projectDesc: 'description',
+        projectLink: 'volunteer.html',
+    },{
+        projectName: 'Clubs',
+        projectImage: 'wave.jpg',
+        projectDesc: 'Learn about my involvement on campus!',
+        projectLink: 'clubs.html',
+    },
+    {
+        projectName: 'Work',
+        projectImage: 'wave.jpg',
+        projectDesc: 'description',
+        projectLink: 'https://github.com/',
+    }
+]
